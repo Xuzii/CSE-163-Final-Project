92980f95be6afcdd04450c5ae9ef380119cbe978
Kenneth <kenneth.anthony@ruslim.net>
2021-08-16 15:54:40 -0700

    imports

--- olympics.py
+++ olympics.py
@@ -3,4 +3,6 @@ Kenneth Ruslim and Nam Nguyen
 CSE 163 AB
 """
 
-'test'
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
--- olympics_test.py
+++ olympics_test.py
@@ -2,3 +2,13 @@
 Kenneth Ruslim and Nam Nguyen
 CSE 163 AB
 """
+
+from cse163_utils import assert_equals, parse
+
+import olympics
+
+def main():
+    print('hello')
+
+if __name__ == '__main__':
+    main()
